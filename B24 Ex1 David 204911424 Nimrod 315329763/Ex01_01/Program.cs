67cf960fb358e7d5a07cfefaa49538651fb92dbd
NimrodPardess <121054914+NimrodPardess@users.noreply.github.com>
2024-04-27 12:00:22 +0300

    First union of projects

---
+++ B24 Ex1 David 204911424 Nimrod 315329763/Ex01_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex01_01
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // this is the entry point
+            StringBuilder sb = new StringBuilder();
+            sb.Append("I love");
+            sb.Append(" Pardo");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
