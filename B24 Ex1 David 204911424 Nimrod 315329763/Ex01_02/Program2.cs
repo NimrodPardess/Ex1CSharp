bb8b2795e84804bdf16b15ff78912235e03bc700
NimrodPardess <121054914+NimrodPardess@users.noreply.github.com>
2024-04-28 09:47:30 +0300

    Reference Ex3 to Ex2 successfully.

---
+++ B24 Ex1 David 204911424 Nimrod 315329763/Ex01_02/Program2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex01_02
+{
+    public class Program2
+    {
+        static void Main()
+        {
+            // Change this number to print a larger or smaller pattern. The main line is 2 * (input) + 1. For example: 4 as input is main line of 9 astericks.
+            PrintDiamondPattern(4);
+            // Console.WriteLine("Not suppose to print");
+        }
+
+        public static void PrintDiamondPattern(int numberOfAstericksDecider, int i_counter = 0)
+        {
+            if (i_counter > numberOfAstericksDecider)
+            {
+                for (int j = numberOfAstericksDecider - 1; j >= 0; j--)
+                {
+                    Console.WriteLine(new String(' ', 2 * (numberOfAstericksDecider - j)) + String.Join(" ", new String('*', 2 * j + 1).ToCharArray()));
+                    Console.WriteLine();
+                }
+                return;
+            }
+            Console.WriteLine(new String(' ', 2 * (numberOfAstericksDecider - i_counter)) + String.Join(" ", new String('*', 2 * i_counter + 1).ToCharArray()));
+            Console.WriteLine();
+            PrintDiamondPattern(numberOfAstericksDecider, i_counter + 1);
+        }
+
+        // Delete in the end
+        public static void HelloWorld()
+        {
+            Console.WriteLine("HelloWorld");
+        }
+    }
+}
---
+++ B24 Ex1 David 204911424 Nimrod 315329763/Ex01_03/Program3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ex01_02;
+
+namespace Ex01_03
+{
+    internal class Program3
+    {
+        static void Main()
+        {
+            Program2.HelloWorld();
+            Program2.PrintDiamondPattern(1);
+        }
+    }
+}
