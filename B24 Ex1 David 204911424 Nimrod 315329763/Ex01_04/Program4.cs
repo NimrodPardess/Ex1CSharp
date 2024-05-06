5caa4bab30bd073d234276055ac2ade56bdbf465
dp25 <david.pardo60@gmail.com>
2024-05-06 12:23:34 +0300

    ex04 is complete. string format + code standards and tryparse are acomplished. NimiN00b

--- B24 Ex1 David 204911424 Nimrod 315329763/Ex01_04/Program4.cs
+++ B24 Ex1 David 204911424 Nimrod 315329763/Ex01_04/Program4.cs
@@ -9,80 +9,65 @@ namespace Ex01_04
     public class Program4
     {
         protected static string m_UserInput;
+        protected static string m_InputIsPalindrome;
+        protected static string m_DivisibleByFour;
+        protected static string m_NumberOfLowerCaseLetters;
 
         public static void Main(String[] args)
         {
             getUserInput();
-            CheckIfPalindrome();
-            CheckIfDivisibleByFour();
-            CountLowerCaseCharacters();
+            checkIfPalindrome();
+            checkIfDivisibleByFour();
+            numberOfLowerCaseCharacters();
+            printResult();
         }
-
-        private static void CheckIfPalindrome()
-        {
-            bool isPalindrome = IsPalindrome(m_UserInput, 0, m_UserInput.Length - 1);
-            if (isPalindrome)
-            {
-                Console.WriteLine($"{m_UserInput} is a Palindrome.");
-            }
-            else
-            {
-                Console.WriteLine($"{m_UserInput} is NOT a Palindrome.");
-            }
-        }
-
-        private static void CheckIfDivisibleByFour()
-        {
-            if (IsAlldigits(m_UserInput))
-            {
-                bool isDivisableByFour = IsDivisableByFour(long.Parse(m_UserInput));
-                if (isDivisableByFour)
-                {
-                    Console.WriteLine($"{m_UserInput} is divisable by 4.");
-                }
-            }
-            else
-            {
-                Console.WriteLine($"{m_UserInput} is NOT divisable by 4.");
-            }
-        }
-
-        private static void CountLowerCaseCharacters()
-        {
-            if (IsAllLetters(m_UserInput))
-            {
-                int lowerCaseCount = CountLowerCaseCharacters(m_UserInput);
-                Console.WriteLine($"{m_UserInput} has {lowerCaseCount} lowercase characters.");
-            }
-            else
-            {
-                Console.WriteLine($"{m_UserInput} is not an English string.");
-            }
-        }
-
         private static void getUserInput()
         {
             Console.WriteLine("Please insert an input of 10 characters:");
             m_UserInput = Console.ReadLine();
 
-            while (!IsValidInput(m_UserInput))
+            while (!isValidInput(m_UserInput))
             {
                 Console.WriteLine("Invalid input. Please insert an input of 10 characters:");
                 m_UserInput = Console.ReadLine();
             }
         }
 
-        private static bool IsValidInput(string i_UserInput)
+        private static void checkIfPalindrome()
+        {
+            bool isPalindrome = Program4.isPalindrome(m_UserInput, 0, m_UserInput.Length - 1);
+            m_InputIsPalindrome = $"{(isPalindrome ? "is a palindrome." : "is Not a palindrome.")}";
+        }
+
+        private static void checkIfDivisibleByFour()
+        {
+            if (IsAlldigits(m_UserInput))
+            {
+                bool isDivisableByFour = Program4.isDivisableByFour(long.Parse(m_UserInput));
+                m_DivisibleByFour = $"{(isDivisableByFour ? "is divisible by 4." : "is Not divisible by 4.")}";
+            }
+        }
+
+        private static void numberOfLowerCaseCharacters()
+        {
+            if (isAllLetters(m_UserInput))
+            {
+                int lowerCaseCount = countLowerCaseCharacters(m_UserInput);
+                m_NumberOfLowerCaseLetters = $"The input {m_UserInput} has {lowerCaseCount} lowercase characters.";
+            }
+        }
+
+        private static bool isValidInput(string i_UserInput)
         {
             if (i_UserInput.Length != 10)
             {
                 return false;
             }
 
-            return IsAlldigits(i_UserInput) || IsAllLetters(i_UserInput);
+            return IsAlldigits(i_UserInput) || isAllLetters(i_UserInput);
         }
 
-        private static bool IsAllLetters(string i_input)
+        private static bool isAllLetters(string i_input)
         {
             foreach (char letter in i_input)
             {
@@ -97,18 +82,11 @@ namespace Ex01_04
 
         public static bool IsAlldigits(string i_input)
         {
-            foreach (char digit in i_input) 
-            {
-                if (!char.IsDigit(digit))
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            bool allCharactersAreDigits = long.TryParse(i_input, out long number);
+            return allCharactersAreDigits;
         }
 
-        private static bool IsPalindrome(string i_input, int i_StartOfInput, int i_EndOfInput)
+        private static bool isPalindrome(string i_input, int i_StartOfInput, int i_EndOfInput)
         {
             // Base case
             if (i_StartOfInput >= i_EndOfInput)
@@ -118,13 +96,13 @@ namespace Ex01_04
 
             if (i_input[i_StartOfInput] == i_input[i_EndOfInput])
             {
-                return IsPalindrome(i_input, i_StartOfInput + 1, i_EndOfInput - 1);
+                return isPalindrome(i_input, i_StartOfInput + 1, i_EndOfInput - 1);
             }
 
             return false;
         }
 
-        private static bool IsDivisableByFour(long i_Num)
+        private static bool isDivisableByFour(long i_Num)
         {
             if (i_Num % 4 == 0)
             {
@@ -134,7 +112,7 @@ namespace Ex01_04
             return false;
         }
 
-        private static int CountLowerCaseCharacters(string i_input)
+        private static int countLowerCaseCharacters(string i_input)
         {
             int lowerCaseCount = 0;
 
@@ -146,6 +124,14 @@ namespace Ex01_04
                 }
             }
             return lowerCaseCount;
+        }
+
+        private static void printResult()
+        {
+            string result = string.Format("The input {0} {1}\n" +
+                                          $"{(IsAlldigits(m_UserInput) ? "The input {0} {2}\n" : "{3}\n")}"
+                                          , m_UserInput, m_InputIsPalindrome,m_DivisibleByFour, m_NumberOfLowerCaseLetters);
+            Console.WriteLine(result);  
         }
 
     }
